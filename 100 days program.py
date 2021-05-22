90f734330fe3f87379af9dbf83ff051c3f5c82b4
Nagendrasai9 <69496169+Nagendrasai9@users.noreply.github.com>
2021-05-22 09:27:11 +0530

    Add files via upload

--- 100 days program.py
+++ 100 days program.py
@@ -130,16 +130,195 @@
 
 
 
-num,s,re=map(int,input().split())
-count=0
-x=0
-while num:
-     r=num%10
-     num=num//10
-     if r==s:
-          r=re
-     elif r%s==0:
-          r=(r//s)
-     x=x+r*pow(10,count)
-     count+=1
-print(x)
+##num,s,re=map(int,input().split())
+##count=0
+##x=0
+##while num:
+##     r=num%10
+##     num=num//10
+##     if r==s:
+##          r=re
+##     elif r%s==0:
+##          r=(r//s)
+##     x=x+r*pow(10,count)
+##     count+=1
+##print(x)
+
+
+
+
+##a,b,c,d=map(int,input().split())
+##d=int(input())
+##a=0
+##b=1
+##print(a)
+##for i in range (1,d+1):
+##    c=a+b
+##    a=b
+##    b=c
+##    print(c)
+##    
+
+
+
+##def fib(n):
+##    if n==0:
+##        return True
+##    if n==1:
+##        print(0)
+##        return True
+##    a,b,c=0,1,0
+##      #print(a,b,end=" ")
+##    i=3
+##    p=1
+##    while True:
+##        c=a+b
+##        if c==n:
+##            print(p)
+##        if c>n:
+####            if (n-b)>=(c-n):
+####                print(b)
+####            if (n-b)>=(c-n):
+####                print(c)
+####            return False
+##        a =b
+##        b=c
+##        p+=1
+##        print(p)
+##        
+##    else:
+##        return False
+##n=int(input())
+##print(fib(n))
+
+
+
+
+##def mul(a,b):
+##    if a==1:
+##        return b
+##    if a%2:
+##        return b+mul(a//2,b*2)
+##    else:
+##        return 0+mul(a//2,b*2)
+##a,b=map(int,input().split())
+##print(mul(a,b))
+
+
+
+##def fib(a,b,d,n):
+##    if d>n:
+##        return
+##    if d==1:
+##        print(a,end=' ')
+##        d+1
+##    if d==2:
+##        print(b,end=' ')
+##        d+1
+##    print(a+b,end=' ')
+##    fib(b,a+b,d+1,n)
+##    
+##n=int(input())
+##fib(0,1,1,n-2)
+
+
+
+##def arm(n):
+##    temp=n
+##    dc=0
+##    while temp:
+##        temp=temp//10
+##        dc+=1
+##    res=0
+##    temp=n
+##    while temp:
+##        r=temp%10
+##        temp=temp//10
+##        res+=r**dc
+##    print(dc)
+##    if n==res:
+##        return True
+##    return False
+##
+##n=int(input())
+##print(arm(n))
+
+
+##import math
+##def per(num):
+##    res=1
+##    s=int(math.sqrt(num))
+##    for i in range (2,s+1):
+##        if num%i==0:
+##            res+=i+num//i
+##    return res==num
+##num=int(input())
+##print(per(num))
+        
+
+
+
+##def lcm(a,b):
+##    t=2
+##    res=1
+##    
+##    while a>=t and b>=t:
+##        
+##        if a%t==0 and b%t==0:
+##            
+##            a=a//t
+##            b=b//t
+##            res=res*t
+##        else:
+##            t=t+1       
+##    return res*a*b
+##            
+##            
+##            
+##        
+##        
+##a,b=map(int,input().split())
+##print(lcm(a,b))
+
+
+            
+        
+##def lcm(a,b):
+##    if a<b:
+##        a,b=b,a
+##        m=a
+##        while True:
+##            if m%a==0 and m%b==0:
+##                return m
+##            else:
+##                m+=1
+##a,b=map(int,input().split())
+##print(lcm(a,b))
+
+
+
+##def gcd(a,b):
+##    if a>b:
+##        a,b=b,a
+##        return a
+##    else:
+##        return gcd(a,b%a)
+##a,b=map(int,input().split())
+##print(gcd(a,b))
+##
+##
+
+
+from math import *
+def fac(a):
+    s=round(sqrt(a))
+    b=2
+    for i in range (2,s+1):
+        if a%i==0:
+            if i==a//i:
+                b+=1
+            else:
+                b+=2
+    print(b)
+a=int(input())
+fac(a)
